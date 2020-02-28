b5c14c8d39c42e0b2ea843186fcaac4869b5f09c
= <mehdi429@outlook.com>
2020-02-28 17:20:35 +0000

    Added get request to homepage

--- ChitterApp/App.js
+++ ChitterApp/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { FlatList, ActivityIndicator, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createDrawerNavigator } from '@react-navigation/drawer';
@@ -11,10 +10,16 @@ import SearchScreen from './screens/SearchScreen';
 const Tab = createBottomTabNavigator();
 const Drawer = createDrawerNavigator();
 
-export default function App() {
+export default function ChitterApp() {
+	return (
+		<AppTabNav />
+	)
+}
+
+function AppDrawerNav() {
 	return (
 		<NavigationContainer>
-			<Drawer.Navigator initialRouteName="Home">
+			<Drawer.Navigator>
 				<Drawer.Screen name="Home" component={AppTabNav} />
 			</Drawer.Navigator>
 		</NavigationContainer>
@@ -23,34 +28,36 @@ export default function App() {
 
 function AppTabNav() {
 	return (
-		<Tab.Navigator>
-			<Tab.Screen
-				options={{
-					tabBarIcon: ({ color }) => <Icon
-						name="home"
-						size={30}
-						color={color} />
-				}}
-				name="Home" component={HomeScreen}
-			/>
-			<Tab.Screen
-				options={{
-					tabBarIcon: ({ color }) => <Icon
-						name="plus"
-						size={30}
-						color={color} />
-				}}
-				name="Post" component={CreateChitScreen}
-			/>
-			<Tab.Screen
-				options={{
-					tabBarIcon: ({ color }) => <Icon
-						name="search"
-						size={30}
-						color={color} />
-				}}
-				name="Search" component={SearchScreen}
-			/>
-		</Tab.Navigator>
+		<NavigationContainer>
+			<Tab.Navigator>
+				<Tab.Screen
+					options={{
+						tabBarIcon: ({ color }) => <Icon
+							name="home"
+							size={30}
+							color={color} />
+					}}
+					name="Home" component={HomeScreen}
+				/>
+				<Tab.Screen
+					options={{
+						tabBarIcon: ({ color }) => <Icon
+							name="plus"
+							size={30}
+							color={color} />
+					}}
+					name="Post" component={CreateChitScreen}
+				/>
+				<Tab.Screen
+					options={{
+						tabBarIcon: ({ color }) => <Icon
+							name="search"
+							size={30}
+							color={color} />
+					}}
+					name="Search" component={SearchScreen}
+				/>
+			</Tab.Navigator>
+		</NavigationContainer>
 	)
 }
--- ChitterApp/screens/HomeScreen.js
+++ ChitterApp/screens/HomeScreen.js
@@ -1,13 +1,93 @@
 import React, { Component } from 'react';
-import { Text, View, Button } from 'react-native';
+import { ScrollView, ActivityIndicator, Text, View, StyleSheet, Button } from 'react-native';
+import { createStackNavigator } from '@react-navigation/stack';
+import LoginScreen from './LoginScreen';
 
-class HomeScreen extends Component {
+const Stack = createStackNavigator();
+
+function HomeStackNav() {
+	return (
+		<Stack.Navigator>
+			<Stack.Screen name="Login" component={LoginScreen} />
+		</Stack.Navigator>
+	);
+}
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		backgroundColor: 'lightgrey',
+	},
+	item: {
+		marginTop: 12,
+		padding: 30,
+		backgroundColor: 'white',
+		fontSize: 18,
+	},
+});
+
+export default class HomeScreen extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			isLoading: true,
+			shoppingListData: []
+		}
+	}
+
+	getData() {
+		return fetch('http://10.0.2.2:3333/api/v0.0.5/chits')
+			.then((response) => response.json())
+			.then((responseJson) => {
+				this.setState({
+					isLoading: false,
+					shoppingListData: responseJson,
+				});
+			})
+			.catch((error) => {
+				console.log(error);
+			});
+	}
+
+	componentDidMount() {
+		this.getData();
+	}
+
 	render() {
+		if (this.state.isLoading) {
+			return (
+				<View>
+					<ActivityIndicator />
+				</View>
+			)
+		}
 		return (
-			<View>
+			<Stack.Navigator>
+				<Stack.Screen name="Login" component={LoginScreen} 
+				options = {{
+					headerRight: () => (
+						<Button
+						  onPress={() => alert('This is a button!')}
+						  title="Info"
+						  color="#fff"
+						/>
+					  ),
+				}}
+				/>
+			</Stack.Navigator>,
 
+			<View style={styles.container}>
+				<ScrollView>
+					{this.state.shoppingListData.map((item) => {
+						return (
+							<View key={item.chit_id}>
+								<Text style={styles.item}>{item.user.given_name}{"\n\n"}{item.chit_content}</Text>
+							</View>
+						)
+					})}
+
+				</ScrollView>
 			</View>
 		);
 	}
 }
-export default HomeScreen;
