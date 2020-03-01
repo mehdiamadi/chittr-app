a1d1ba0586592286f836e6c15655c0c85400aaec
mehdi80 <mehdi429@outlook.com>
2020-03-01 16:11:02 +0000

    Implemented followers and following end point for user screen@

--- ChitterApp/screens/UserScreen.js
+++ ChitterApp/screens/UserScreen.js
@@ -1,5 +1,75 @@
 import React, { Component } from 'react';
-import { ActivityIndicator, Text, View, StyleSheet, Alert } from 'react-native';
+import { ActivityIndicator, Text, View, StyleSheet, Alert, ScrollView, TouchableOpacity } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    item: {
+        marginTop: 12,
+        padding: 30,
+        backgroundColor: 'white',
+        fontSize: 18,
+    },
+});
+
+function FollowersScreen({ route }) {
+    const { followersData } = route.params;
+    return (
+        <View>
+            <ScrollView>
+                {followersData.map((item) => {
+                    return (
+                        <View key={item.user_id}>
+                            <TouchableOpacity
+                                // onPress={() => {
+                                //     this.props.navigation.navigate('User', {
+                                //         userID: item.user_id,
+                                //     });
+                                // }}
+                                >
+                                <View style={styles.item}>
+                                    <Text>{item.given_name}</Text>
+                                </View>
+                            </TouchableOpacity>
+                        </View>
+                    )
+                })}
+            </ScrollView>
+        </View >
+    );
+}
+
+function FollowingScreen({ route }) {
+    const { followingData } = route.params;
+    return (
+        <View>
+            <ScrollView>
+                {followingData.map((item) => {
+                    return (
+                        <View key={item.user_id}>
+                            <TouchableOpacity
+                                // onPress={() => {
+                                //     this.props.navigation.navigate('User', {
+                                //         userID: item.user_id,
+                                //     });
+                                // }}
+                                >
+                                <View style={styles.item}>
+                                    <Text>{item.given_name}</Text>
+                                </View>
+                            </TouchableOpacity>
+                        </View>
+                    )
+                })}
+            </ScrollView>
+        </View>
+    );
+}
+
+const Tab = createMaterialTopTabNavigator();
 
 export default class UserScreen extends Component {
     constructor(props) {
@@ -7,7 +77,9 @@ export default class UserScreen extends Component {
         this.state = {
             isLoading: true,
             userID: '',
-            given_name: ''
+            given_name: '',
+            followers: [],
+            following: []
         }
     }
 
@@ -25,9 +97,39 @@ export default class UserScreen extends Component {
             });
     }
 
+    getFollowers(userID) {
+        return fetch('http://10.0.2.2:3333/api/v0.0.5/user/' + userID + '/followers')
+            .then((response) => response.json())
+            .then((responseJson) => {
+                this.setState({
+                    isLoading: false,
+                    followers: responseJson,
+                });
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }
+
+    getFollowing(userID) {
+        return fetch('http://10.0.2.2:3333/api/v0.0.5/user/' + userID + '/following')
+            .then((response) => response.json())
+            .then((responseJson) => {
+                this.setState({
+                    isLoading: false,
+                    following: responseJson,
+                });
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }
+
     componentDidMount() {
         const { userID } = this.props.route.params;
         this.getUser(userID);
+        this.getFollowers(userID);
+        this.getFollowing(userID);
     }
 
     render() {
@@ -39,9 +141,10 @@ export default class UserScreen extends Component {
             )
         }
         return (
-            <View>
-                <Text>{this.state.given_name}</Text>
-            </View>
+            <Tab.Navigator>
+                <Tab.Screen name="Followers" component={FollowersScreen} initialParams={{ followersData: this.state.followers }} />
+                <Tab.Screen name="Following" component={FollowingScreen} initialParams={{ followingData: this.state.following }} />
+            </Tab.Navigator>
         );
     }
 }
